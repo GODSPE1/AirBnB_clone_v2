8de12d46c83d406878e6ea7311718e835506644c
GODSPE1 <godspy18@gmail.com>
2024-04-22 00:15:23 +0100

    implemented pycodestyle

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,16 +13,19 @@ def hello():
     """Displays Hello HBNB!"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays HBNB"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Displays C followed by the value of the text variable"""
     text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
