8c6f63e2c7e7a9822708852ba0b83afd27153880
GODSPE1 <godspy18@gmail.com>
2024-04-22 00:10:01 +0100

    implemented pycodestyle

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -14,10 +14,12 @@ def hello():
     """Displays Hello HBNB!"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays HBNB"""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
@@ -25,17 +27,18 @@ def c(text):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
     """Displays python followed by the value of the text variable"""
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
     """Display “n is a number” only if n is an integer"""
     return "{} is a number".format(n)
-
 
 
 if __name__ == "__main__":
